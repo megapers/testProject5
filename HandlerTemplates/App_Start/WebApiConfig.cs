eda0486b24a6d176d460bf4451fea5d7e66d0275
TMakimov <TMakimov@DTMAKIMOVW10>
2020-01-10 16:21:07 -0800

    init

---
+++ HandlerTemplates/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using TestProject5.Filters;
+using System.Web.Http;
+
+namespace HandlerTemplates
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+            // Web API configuration and services
+            config.Filters.Add(new RouteTimerFilterAttribute("Global"));
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            // Remove the template routing
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional },
+            //    constraints: new { id = new RegexBasedConstraintTemplate() },
+            //    handler: new DelegatingHandlerTemplate()
+            //);
+        }
+    }
+}
